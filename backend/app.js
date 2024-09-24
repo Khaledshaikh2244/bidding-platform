ace0f2a2d03a843bc5518ef7b3b9221ff29ed9c3
Khaledshaikh2244 <khankhaled501@gmail.com>
2024-09-24 17:33:04 +0530

    file missing issue resolved

--- backend/app.js
+++ backend/app.js
@@ -1,9 +1,30 @@
 import { config } from 'dotenv';
 import express from 'express';
+import { connection } from './database/conneciton.js';
+import cors from 'cors';
+import cookieParser from 'cookie-parser';
+import fileUpload from 'express-fileupload';
+
 
 const app = express();
 config({
     path : "./config/config.env"
 })
 
+
+    app.use(cors({
+        origin : [process.env.FRONTEND_URI],
+        methods : ["POST","GET","PUT", "DELETE"],
+        credentials : true,
+    }))
+    
+    app.use(cookieParser());
+    app.use(express.json);
+    app.use(express.urlencoded({extended :true}));
+    app.use(fileUpload({
+        useTempFiles : true,
+        tempFileDir : "/temp/",
+    }))
+
+connection()
 export default app;
