cde7ae6bea00eb1e153f16ed464300d0f3a8bfda
Khaledshaikh2244 <khankhaled501@gmail.com>
2024-09-25 18:34:43 +0530

    added more user routes

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -1,8 +1,12 @@
 import express from 'express';
-import {registerController} from '../controllers/userController.js'
+import {leaderboardController, getProfileController, loginController, logoutController, registerController} from '../controllers/userController.js'
 
 const router = express.Router();
 
 router.post("/register", registerController);
+router.post("/login",loginController);
+router.get("/me",getProfileController)
+router.get("/logout",logoutController);
+router.get("/leaderboard",leaderboardController);
 
 export default router;
