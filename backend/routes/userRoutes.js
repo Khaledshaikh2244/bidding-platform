67466e3d315a32f310918f4295c185d6e2160984
Khaledshaikh2244 <khankhaled501@gmail.com>
2024-09-25 15:47:36 +0530

    updated register with registerController

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -1,8 +1,8 @@
 import express from 'express';
-import {register} from '../controllers/userController.js'
+import {registerController} from '../controllers/userController.js'
 
 const router = express.Router();
 
-router.post("/register", register);
+router.post("/register", registerController);
 
 export default router;
