91c8eff6fdac64ed76c1581248e3155f436ace4a
Khaledshaikh2244 <khankhaled501@gmail.com>
2024-09-25 11:39:52 +0530

    fixed dotenv conflict

--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,4 @@
 import app from './app.js';
-import { config } from 'dotenv';
 import {v2 as cloudinary } from 'cloudinary';
 
 
@@ -8,7 +7,6 @@ cloudinary.config({
     api_key :  process.env.CLOUDINARY_API_KEY,
     api_secret : process.env.CLOUDINARY_API_SECRET,
     })
-    
 
 app.listen(process.env.PORT, () => {
     console.log(`server is listening on port ${process.env.PORT}`);
