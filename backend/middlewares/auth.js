503b34227013146e173aab526e0a80013c1bf08a
Khaledshaikh2244 <khankhaled501@gmail.com>
2024-09-26 15:07:51 +0530

    added some more  error handling

--- backend/middlewares/auth.js
+++ backend/middlewares/auth.js
@@ -3,15 +3,26 @@ import { catchAsynchError } from "./catchAsyncError.js";
 import {User}  from '../models/userSchema.js';
 import jwt from 'jsonwebtoken';
 
-
 const isAuthenticated = async (req,res,next) => {
     const token = req.cookies.token;
+    const isLogout = req.path === '/logout';
 
     if(!token){
-        return next(new ErrorHandler("User not authenticated"),400);
+        const errorMessage = isLogout ? 'Aleady logged out' : 'User not authenticated';
+        return next(new ErrorHandler(errorMessage,400));
     }
 
     const decoded =  jwt.verify(token, process.env.JWT_SECRET);
+    console.log(decoded);
+    if(!decoded || !decoded.id){
+      return next(new ErrorHandler("Invalid Token",401));
+    }
+
     req.user = await User.findById(decoded.id);
+    if(!req.user){
+        return next(new ErrorHandler("User not found",404));
+    }
     next();
 }
+
+export const authMiddleware = catchAsynchError(isAuthenticated);
