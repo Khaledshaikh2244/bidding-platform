0d79f8ce69e97870d327e1345017b2e69fbd65dd
Khaledshaikh2244 <khankhaled501@gmail.com>
2024-09-26 23:27:44 +0530

    typo error resolved

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -149,7 +149,7 @@ export const logoutController = catchAsynchError(logout);
 
 const fetchLeaderBoard = async (req,res,next) => {
   const users = await User.find({moneySpent: {$gt:0}});
-  leaderboard = users.sort((a,b) => b.moneySpent - a.moneySpent);
+  const leaderboard = users.sort((a,b) => b.moneySpent - a.moneySpent);
   res.status(200).json({
     success : true,
     leaderboard,
