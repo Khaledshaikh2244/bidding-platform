6f5320b303118c3a4c0d5df0f2d48e9bfbd8e7cc
Khaledshaikh2244 <khankhaled501@gmail.com>
2024-09-26 12:59:11 +0530

    added authMiddleware on  "/me" route

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -1,11 +1,11 @@
 import express from 'express';
 import {leaderboardController, getProfileController, loginController, logoutController, registerController} from '../controllers/userController.js'
-
+import {authMiddleware} from '../middlewares/auth.js'
 const router = express.Router();
 
 router.post("/register", registerController);
 router.post("/login",loginController);
-router.get("/me",getProfileController)
+router.get("/me",authMiddleware,getProfileController)
 router.get("/logout",logoutController);
 router.get("/leaderboard",leaderboardController);
 
