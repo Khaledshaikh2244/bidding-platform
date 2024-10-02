43f7a3304bb556196a7eb6bc501a7e0937a004ef
Khaledshaikh2244 <khankhaled501@gmail.com>
2024-10-02 14:03:02 +0530

    added more auctionRoutes

--- backend/routes/aucitonRoutes.js
+++ backend/routes/aucitonRoutes.js
@@ -1,4 +1,4 @@
-import {auctionController} from "../controllers/auctionController.js"
+import {auctionController, getAllItemsController, getAuctionDetailsController, removeFromAuctionController, republishItemsController} from "../controllers/auctionController.js"
 import {authMiddleware, authorizedMidleware} from "../middlewares/auth.js"
 import express from "express";
 
@@ -7,6 +7,11 @@ const router = express.Router();
 
 
 router.post("/create",authMiddleware,authorizedMidleware("Auctioneer"),auctionController);
+router.get("/allitems",getAllItemsController);  
+router.get("/auction/:id",getAuctionDetailsController);  
+router.delete("delete/:id",authMiddleware, authorizedMidleware("Auctioneer"),removeFromAuctionController);
 
+router.put("/item/republish/:id",authMiddleware, authorizedMidleware("Auctioneer"),republishItemsController)
 
 export default router;
+
